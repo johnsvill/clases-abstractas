7016e523142e9ddeb6f5981bfa13d08c084ce015
Jonathan Villeda <johnsvill@hotmail.com>
2019-03-26 22:59:05 -0600

    Subiendo primera versión

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesAbstractas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            clsClientesBase cliente = new clsClientesBase(0, "Jonathan Villeda",
+                "001",
+                "JSVP",
+                1, "Jonathan Villeda");
+            Console.WriteLine(cliente.Clave + " " + cliente.Nombre);
+            Console.WriteLine(cliente.TipoRegimen);
+            Console.WriteLine(cliente.RFC);
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ clsClientesBase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesAbstractas
+{
+    public class clsClientesBase : clsAbsClientes
+    {
+        public clsClientesBase()
+        {
+            Id = 0;
+            Nombre = string.Empty;
+            RFC = string.Empty;
+            Clave = string.Empty;
+            TipoRegimen = 0;
+            NombreContacto = string.Empty;
+        }
+        //Pueden haber 2 métodos con el mismo nombre pero la diferencia son los parámetros q reciben cada uno
+        public clsClientesBase(int pId, string pNombre, string pClave,
+           string pRFC, int pTipoRegimen, string pNombreContacto)
+        {
+            Id = pId;
+            Nombre = pNombre;
+            RFC = pRFC;
+            Clave = pClave;
+            TipoRegimen = pTipoRegimen;
+            NombreContacto = pNombreContacto;
+        }
+        public override int Id { get; set; }//Override es para implementar una propiedad q viene de una clase abstracta
+        public override string Nombre { get; set; }
+        public override string Clave { get; set; }
+        public override string RFC { get; set; }
+        public override int TipoRegimen { get; set; }
+        public override string NombreContacto { get; set; }
+    }
+}
